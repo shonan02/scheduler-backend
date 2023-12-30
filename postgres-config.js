c855cc211631a2393ca7aeb6b42e86ef4c9420fd
shonan02 <66680460+shonan02@users.noreply.github.com>
2023-12-30 17:50:17 +0000

    Create postgres pool

---
+++ postgres-config.js
@@ -0,0 +1,14 @@
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    //Database configuration
+    user: "personal",
+    host: "localhost",
+    database: "scheduling",
+    password: "post-root",
+    port: 5432
+});
+
+module.exports = {
+    pool
+}

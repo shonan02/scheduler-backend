4a8eefab34c5179fc39fd444dfdd19da13c780a6
shonan02 <66680460+shonan02@users.noreply.github.com>
2023-12-31 14:38:37 +0000

    implement password hash

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
 const db = require("../db/postgres-config");
 
+const bcrypt = require('bcrypt');
+
 //Get user by id
 const getUser =(req, res) => {
     const id = parseInt(req.params.id);
@@ -32,17 +34,24 @@ const getUserByUsername = (req,res) => {
 
 
 //Create user query
-const createUser = (req, res) => {
+const createUser = async (req, res) => {
     const { username, email, password } = req.body;
 
-    db.pool.query("INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *;",
-        [username, email, password],
+    //Hash the password 
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+    const currentDate = new Date();
+    const created = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')} ${currentDate.getHours().toString().padStart(2, '0')}:${currentDate.getMinutes().toString().padStart(2, '0')}:${currentDate.getSeconds().toString().padStart(2, '0')}`;
+    console.log(created);
+    db.pool.query("INSERT INTO users (username, email, password, created) VALUES ($1, $2, $3, $4) RETURNING *;",
+        [username, email, passwordHash, created],
         (err, result) => {
             if(err) {
-                throw err;
+                res.status(400).json({error: err});
+                return;
             }
 
-            res.status(201).send("user added.");
+            res.status(201).json({message: 'success'});
         })
 }
 

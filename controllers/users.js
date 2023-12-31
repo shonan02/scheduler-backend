336fcbf9db332021a5531ce6a00e15b9be4deee5
shonan02 <66680460+shonan02@users.noreply.github.com>
2023-12-31 12:18:57 +0000

    Get User by specific id

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,25 @@
-const db = require("./postgres-config");
+const db = require("../postgres-config");
+
+//Get all users
+const getUser =(req, res) => {
+    const id = parseInt(req.params.id);
+
+    //Create query 
+    db.pool.query('SELECT * FROM users WHERE id=$1',
+        [id],
+        (err, results) => {
+            if(err) {
+                throw err;
+            }
+            res.status(200).json(results.rows);
+        })
+}
 
 //Create user query
 const createUser = (req, res) => {
     const { username, email, password } = req.body;
 
-    db.pool.query("INSERT INTO users (username, email, password) VALUES ($username, $email, $password) RETURNING *;",
+    db.pool.query("INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *;",
         [username, email, password],
         (err, result) => {
             if(err) {
@@ -16,5 +31,6 @@ const createUser = (req, res) => {
 }
 
 module.exports = {
-    createUser
+    createUser,
+    getUser
 }

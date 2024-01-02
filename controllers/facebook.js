a9bd9597958037bc75d7d2de273ccb77474be99f
shonan02 <66680460+shonan02@users.noreply.github.com>
2024-01-02 12:54:15 +0000

    create save facebook user token function

--- controllers/facebook.js
+++ controllers/facebook.js
@@ -1,12 +1,21 @@
 const db = require("../db/postgres-config");
 
+//Save facebook user access token 
 const saveFacebookUser = async (req, res) => {
-    //Save the facebook user to the Facebook database
-    db.pool.query("INSERT INTO Facebook (facebook_username, facebook_access_key) VALUES ($1, $2);",
-    [],
-    (err, result) => {
+    const { userId, userDisplayName, accessToken } = req.body;
 
-    });
+    //Create query
+    const query = "INSERT INTO Facebook (user_id, facebook_username, facebook_access_key) VALUES ($1, $2, $3);";
+
+    db.pool.query(query,
+        [userId, userDisplayName, accessToken],
+        (err, results) => {
+            if(err) {
+                return res.status(400).json({ err: err});
+            }
+
+            res.status(201).json({ message: "successful"});
+        });
 }
 
 module.exports = {
